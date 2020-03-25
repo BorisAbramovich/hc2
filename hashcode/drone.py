114fdd6f5f43f19fec10a65a7d3299cb6b3a2cdc
elkon <matan.elko@gmail.com>
2020-03-25 13:39:58 +0200

    complete drone and fixed_warehouse integrated

--- hashcode/drone.py
+++ hashcode/drone.py
@@ -1,23 +1,50 @@
 class Drone(object):
 
-    def __init__(self, number_of_product_types, initial_location, max_load):
+    def __init__(self, id, number_of_product_types, initial_location, max_load):
+        self.id = id
         self.list_of_products = [0] * number_of_product_types
         self.current_load = 0
         self.loc = initial_location
         self.max_load = max_load
+        self.list_of_commands = []
 
-    def load(self, product_type, product_weight, number_of_products):
-        assert self.current_load + number_of_products * product_weight <= self.max_load
-        self.list_of_products[product_type] += number_of_products
-        self.current_load += number_of_products * product_weight
+    def load(self, warehouse, product, number_of_products):
+        dist = self.move(warehouse.loc[0], warehouse.loc[1])
+        self.pack(product, number_of_products)
+        self.list_of_commands.append(' '.join([str(self.id), 'L', str(warehouse.get_id()), str(product.get_id()), str(number_of_products)]) + '\n')
+        return dist
 
-    def deliver(self, new_location):
-        self.loc = new_location
+    def deliver(self, order, product, number_of_products):
+        dist = self.move(order.destination[0], order.destination[1])
+        self.unpack(product, number_of_products)
+        self.list_of_commands.append(' '.join([str(self.id), 'D', str(order.get_id()), str(product.get_id()), str(number_of_products)]) + '\n')
+        return dist
 
-    def unload(self, product_type, product_weight, number_of_products):
-        self.list_of_products[product_type] -= number_of_products
-        assert(self.list_of_products[product_type] >= number_of_products)
-        self.current_load -= number_of_products * product_weight
+    def unload(self, warehouse, product, number_of_products):
+        dist = self.move(warehouse.loc[0], warehouse.loc[1])
+        self.unpack(product, number_of_products)
+        self.list_of_commands.append(' '.join([str(self.id), 'U', str(warehouse.get_id()), str(product.get_id()), str(number_of_products)]) + '\n')
+        return dist
 
-    def wait(self):
-        pass
+    def unpack(self, product, number_of_products):
+        self.list_of_products[product.get_id()] -= number_of_products
+        assert (self.list_of_products[product.get_id()] >= 0)
+        self.current_load -= number_of_products * product.get_weight()
+        assert (self.current_load >= 0)
+
+    def pack(self, product, number_of_products):
+        self.list_of_products[product.get_id()] += number_of_products
+        self.current_load += number_of_products * product.get_weight()
+        assert(self.current_load <= self.max_load)
+
+    def move(self, dest_x, dest_y):
+        dist = int(ceil(((dest_x - self.loc[0]) ** 2 + (dest_y - self.loc[1]) ** 2) ** 0.5))
+        self.loc[0] = dest_x
+        self.loc[1] = dest_y
+        return dist
+
+    def wait(self, number_of_turns):
+        self.list_of_commands.append(' '.join([str(self.id), 'W', str(number_of_turns)]) + '\n')
+
+    def dump_commands(self):
+        return self.list_of_commands
